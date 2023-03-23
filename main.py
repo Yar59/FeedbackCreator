8a877068a2416519047f12a768a3338a65f5fae8
Yar59 <75260156+Yar59@users.noreply.github.com>
2023-03-23 10:33:27 +0300

    splited script to functions

--- main.py
+++ main.py
@@ -84,6 +84,83 @@ PHRASES = {
 
 }
 
+def save_feedback(file_path, feedbacks):
+    writer = pandas.ExcelWriter(file_path, engine='openpyxl', mode='a', if_sheet_exists='overlay')
+    wb = writer.book
+    data_frame = pandas.DataFrame({'ОС': feedbacks})
+    data_frame.to_excel(writer, sheet_name='feedback', index=False, startcol=6)
+    wb.save(file_path)
+
+
+def read_excel(file_path='feedback.xlsx'):
+    return pandas.read_excel(
+        file_path,
+        sheet_name='feedback',
+        usecols=[
+            'ФИО',
+            'Имя для ОС',
+            'Текущий модуль',
+            'Текущий урок',
+            'Статус урока',
+            'Ссылка на девман'
+        ],
+    ).to_dict(orient='records')
+
+
+def generate_feedback(student, dont_use_link=False, manual_additions=False):
+    if student['Ссылка на девман'] and not dont_use_link:
+        print(student['Ссылка на девман'])
+        while True:
+            try:
+                page_content = get_student_page(student['Ссылка на девман'])
+                lesson = get_lesson(page_content)
+                lesson_status = lesson['lesson_status']
+                module_name = lesson['module_name']
+                lesson_number_numerical = int(lesson['lesson_number'])
+                break
+            except requests.exceptions.HTTPError as error:
+                logging.warning(error)
+                return 'Не удалось получить информацию с сайта девман'
+            except requests.exceptions.ConnectionError:
+                logging.warning("Connection Error\nPlease check your internet connection")
+                sleep(5)
+                logging.warning("Trying to reconnect")
+    else:
+        lesson_status = student['Статус урока']
+        module_name = student['Текущий модуль']
+        lesson_number_numerical = student["Текущий урок"]
+    lesson_number = num2words(lesson_number_numerical, to="ordinal", lang="ru")
+    try:
+        module_knowledge = random.choice(MODULES_KNOWLEDGE[module_name][lesson_number_numerical])
+        commendation = random.choice(PHRASES['commendations'])
+        water = random.choice(PHRASES['water'])
+        behaviour = random.choice(PHRASES['behaviour'])
+        end = random.choice(PHRASES['end'])
+        if lesson_status == 'В работе':
+            work_status = 'сейчас выполняет'
+            knowledge_status = random.choice(PHRASES['knowledge_statuses']['in_work'])
+        elif lesson_status == 'Сдается':
+            work_status = 'сейчас сдает'
+            knowledge_status = random.choice(PHRASES['knowledge_statuses']['completed'])
+        else:
+            work_status = 'недавно сдал'
+            knowledge_status = random.choice(PHRASES['knowledge_statuses']['completed'])
+        feedback = dedent(f'''\
+            {student["Имя для ОС"]} {water} {work_status} {lesson_number} проект курса {module_name} 
+            в ходе которого {knowledge_status} {module_knowledge}. {behaviour} {commendation} На занятиях {end}
+        ''')
+        print(feedback)
+
+        if manual_additions:
+            additions = input('Чем дополним ОС?\n')
+            feedback += additions
+            print(feedback, '\n')
+
+        return feedback.replace('\n', ' ')
+    except KeyError:
+        print(f'Модуля {module_name} нет в скрипте, ОС по {student["ФИО"]} не сгенерирована')
+        return 'Текущего модуля нет в скрипте'
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -107,80 +184,15 @@ def main():
     manual_additions = args.manual_additions
     dont_save = args.dont_save
     dont_use_link = args.dont_use_link
-    students = pandas.read_excel(
-        'feedback.xlsx',
-        sheet_name='feedback',
-        usecols=[
-            'ФИО',
-            'Имя для ОС',
-            'Текущий модуль',
-            'Текущий урок',
-            'Статус урока',
-            'Ссылка на девман'
-        ],
-    ).to_dict(orient='records')
+    students = read_excel('feedback.xlsx')
 
     feedbacks = []
     for student in students:
-        if student['Ссылка на девман'] and not dont_use_link:
-            print(student['Ссылка на девман'])
-            while True:
-                try:
-                    page_content = get_student_page(student['Ссылка на девман'])
-                    lesson = get_lesson(page_content)
-                    lesson_status = lesson['lesson_status']
-                    module_name = lesson['module_name']
-                    lesson_number_numerical = int(lesson['lesson_number'])
-                    break
-                except requests.exceptions.HTTPError as error:
-                    logging.warning(error)
-                    break
-                except requests.exceptions.ConnectionError:
-                    logging.warning("Connection Error\nPlease check your internet connection")
-                    sleep(5)
-                    logging.warning("Trying to reconnect")
-        else:
-            lesson_status = student['Статус урока']
-            module_name = student['Текущий модуль']
-            lesson_number_numerical = student["Текущий урок"]
-        lesson_number = num2words(lesson_number_numerical, to="ordinal", lang="ru")
-        try:
-            module_knowledge = random.choice(MODULES_KNOWLEDGE[module_name][lesson_number_numerical])
-            commendation = random.choice(PHRASES['commendations'])
-            water = random.choice(PHRASES['water'])
-            behaviour = random.choice(PHRASES['behaviour'])
-            end = random.choice(PHRASES['end'])
-            if lesson_status == 'В работе':
-                work_status = 'сейчас выполняет'
-                knowledge_status = random.choice(PHRASES['knowledge_statuses']['in_work'])
-            elif lesson_status == 'Сдается':
-                work_status = 'сейчас сдает'
-                knowledge_status = random.choice(PHRASES['knowledge_statuses']['completed'])
-            else:
-                work_status = 'недавно сдал'
-                knowledge_status = random.choice(PHRASES['knowledge_statuses']['completed'])
-            feedback = dedent(f'''\
-                {student["Имя для ОС"]} {water} {work_status} {lesson_number} проект курса {module_name} 
-                в ходе которого {knowledge_status} {module_knowledge}. {behaviour} {commendation} На занятиях {end}
-            ''')
-            print(feedback)
-
-            if manual_additions:
-                additions = input('Чем дополним ОС?\n')
-                feedback += additions
-                print(feedback, '\n')
-
-            feedbacks.append(feedback.replace('\n', ' '))
-        except KeyError:
-            print(f'Модуля {module_name} нет в скрипте, ОС по {student["ФИО"]} не сгенерирована')
-            feedbacks.append('Текущего модуля нет в скрипте')
+        feedback = generate_feedback(student, dont_use_link, manual_additions)
+        feedbacks.append(feedback)
 
     if not dont_save:
-        writer = pandas.ExcelWriter('feedback.xlsx', engine='openpyxl', mode='a', if_sheet_exists='overlay')
-        wb = writer.book
-        data_frame = pandas.DataFrame({'ОС': feedbacks})
-        data_frame.to_excel(writer, sheet_name='feedback', index=False, startcol=6)
-        wb.save('feedback.xlsx')
+        save_feedback('feedback.xlsx', feedbacks)
 
 
 if __name__ == '__main__':
